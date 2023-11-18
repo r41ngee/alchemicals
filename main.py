43cb562cc14f02e477de0ce9b6d104a94535b8e9
r41ngee <r41ngee@yandex.ru>
2023-11-18 23:07:04 +0300

    completed main.py

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
 import elemental
+import interface
+import easygui as gui
 
-pass
+def main():
+    message = 'Выберите действие:'
+    screenTitle = 'Выбор действия'
+
+    button1 = 'Атомная масса'
+    button2 = 'Молекулярная масса'
+    button3 = 'Массовая доля'
+
+    result = gui.buttonbox(msg=message, title=screenTitle, choices=(button1, button2, button3))
+
+    def writeAtomicMass() -> int | float:
+        element = interface.writeElement()
+
+        return element.atomic_mass
+    
+    def writeMolecularMass() -> int:
+        compound = interface.writeCompound()
+
+        return compound.getMolecularMass()
+    
+    def writeMassFraction():
+        compound = interface.writeCompound()
+        element = interface.writeElement()
+
+        return compound.getMassFraction(element.literal, 'percentage')
+    
+    if result == button1:
+        gui.msgbox(str(writeAtomicMass()))
+
+    elif result == button2:
+        gui.msgbox(str(writeMolecularMass()))
+
+    elif result == button3:
+        gui.msgbox(str(writeMassFraction()))
+
+if __name__ == '__main__':
+    main()
